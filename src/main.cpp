5f5740299c11687ea92496a695876803e71ad8ef
Prateek Kumar <prateek@prateekkumar.in>
2018-04-27 19:25:15 +0530

    Call read_goto_binary function in main
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include "interval_domain.h"
-
-using namespace intervalai;
-using namespace std;
+#include <goto-programs/read_goto_binary.h>
+#include <goto-programs/goto_model.h>
+#include <util/cout_message.h>
 
 int main() {
-	Interval interval;
-	cout << "Hello World!" << endl;
+
+	goto_modelt model;
+	cout_message_handlert message_handler;
+
+	bool error = read_goto_binary("../../cbmctests/e.goto", model, message_handler);
+
+	if (error) {
+		std::cerr << "Error!" << std::endl;
+	} else {
+		model.output(std::cout);
+	}
+
 	return 0;
 }
