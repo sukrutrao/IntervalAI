e7f76f80efc7d6b28f8cb241d5578b150c58a3d1
Prateek Kumar <prateek@prateekkumar.in>
2018-04-27 21:27:07 +0530

    Basic main.cpp, prints instructions in main func
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
+#include <string>
 #include "interval_domain.h"
 #include <goto-programs/read_goto_binary.h>
 #include <goto-programs/goto_model.h>
 #include <util/cout_message.h>
 
-int main() {
+int main(int argc, char **argv) {
 
 	goto_modelt model;
 	cout_message_handlert message_handler;
 
-	bool error = read_goto_binary("../../cbmctests/e.goto", model, message_handler);
+	bool error = read_goto_binary(std::string(argv[1]), model, message_handler);
 
 	if (error) {
-		std::cerr << "Error!" << std::endl;
-	} else {
-		model.output(std::cout);
+		return 1;
+	}
+
+	auto instructions = model.goto_functions.function_map["main"].body.instructions;
+
+	for (auto instruction: instructions) {
+		std::cout << instruction.to_string() << std::endl;
 	}
 
 	return 0;
