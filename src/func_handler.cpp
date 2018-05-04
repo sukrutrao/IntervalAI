dc41e7faee163783661ad9aee4edbcecddc4a592
Sukrut Rao <raosukrut@gmail.com>
2018-05-04 05:32:44 +0530

    Update display and README

--- src/func_handler.cpp
+++ src/func_handler.cpp
@@ -71,6 +71,11 @@ bool FuncHandler::handleInstruction(std::_List_iterator<instructiont> current) {
             }
             loop_count[instruction.location_number] += 1;
             if (loop_count[instruction.location_number] >= widen_limit) {
+                std::cout << "Limit on number of iterations (" << widen_limit
+                          << ") reached." << std::endl
+                          << "Widening will be applied. Instruction : "
+                          << instruction.location_number << ", "
+                          << instruction.to_string() << std::endl;
                 auto iter = current;
                 while (iter != instruction.targets.front()) {
                     if ((*iter).is_assign()) {
@@ -93,7 +98,8 @@ bool FuncHandler::handleInstruction(std::_List_iterator<instructiont> current) {
                 std::string input;
                 std::cout << prompt;
                 std::cout << "A branch has been encountered. Instruction : "
-                          << (*current).location_number << std::endl;
+                          << (*current).location_number << ", "
+                          << (*current).to_string() << std::endl;
                 std::cout
                     << "Select an option : (1) Then Branch, (2) Else Branch"
                     << std::endl;
@@ -132,6 +138,8 @@ void FuncHandler::displayInfo(std::_List_iterator<instructiont> current) {
         std::cout << prompt;
         std::cin >> input;
         if (input == "n") {
+            std::cout << "Executed instruction : " << (*current).location_number
+                      << ", " << (*current).to_string() << std::endl;
             return;
         } else if (input == "p") {
             for (auto &sym : instruction_handler.expr_handler.symbol_table) {
@@ -139,7 +147,7 @@ void FuncHandler::displayInfo(std::_List_iterator<instructiont> current) {
                           << std::endl;
             }
             std::cout << "Next instruction : " << (*current).location_number
-                      << std::endl;
+                      << ", " << (*current).to_string() << std::endl;
         } else {
             std::cout << "Invalid command" << std::endl;
         }
